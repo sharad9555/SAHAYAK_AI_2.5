06fa6e672a4637267c39f08be8b3e870886ae768
sharad9555 <mine809053@gmail.com>
2026-09-19 14:36:25 +0530

    reverted the code

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,12 @@ from .database import Base,engine,get_db
 from .models import User,History
 from pydantic import BaseModel
 
-# Base.metadata.create_all(engine)
-# app=FastAPI(title="Sahayak AI")
-# OLLAMA="http://127.0.0.1:11434/api/chat"
-# MODEL="llama3.2:1b"
-# VISION_MODEL="moondream"
+Base.metadata.create_all(engine)
+app=FastAPI(title="Sahayak AI")
+OLLAMA="http://127.0.0.1:11434/api/chat"
+MODEL="llama3.2:1b"
+name="sharad"
+VISION_MODEL="moondream"
 
 class UserCreate(BaseModel):
     name:str
